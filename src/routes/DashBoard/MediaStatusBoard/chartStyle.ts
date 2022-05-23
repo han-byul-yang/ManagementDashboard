65957dc9cbfdc533f03e9d191bcb2b5b0b328aa6
Han Byul <yahb9@naver.com>
2022-05-24 06:59:14 +0900

    Design: Styling chart

---
+++ src/routes/DashBoard/MediaStatusBoard/chartStyle.ts
@@ -0,0 +1,12 @@
+const chartStyle = {
+  bar: {
+    style: {
+      data: { stroke: '#FFFFFF', strokeWidth: 1 },
+    },
+    barWidth: 30,
+    x: 'category',
+    y: 'value',
+  },
+}
+
+export default chartStyle
