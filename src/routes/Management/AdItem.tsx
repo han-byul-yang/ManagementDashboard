848af926dd59f5b9309246a6fbc38b66cdae99b3
lazy-sky <1cielo@naver.com>
2022-05-24 00:37:32 +0900

    Feat: Can toggle ad status during editting mode

--- src/routes/Management/AdItem.tsx
+++ src/routes/Management/AdItem.tsx
@@ -23,17 +23,40 @@ const AdItem = ({ ad }: { ad: IAdCard }) => {
 
   const [adList, setAdList] = useRecoilState<IAdCard[]>(adListState)
 
+  // 어떤 요소까지 수정이 가능한지 주어진 데이터만으로는 알기 어려워 임의적으로 제목과 예산만 수정 가능한 요소라고 판단
   const handleEditBtnClick = (targetId: number) => {
-    setAdList(adList.map((item: any) => (targetId === item.id ? { ...item, isEditting: !ad.isEditting } : item)))
+    setAdList(adList.map((item: IAdCard) => (targetId === item.id ? { ...item, isEditting: !ad.isEditting } : item)))
+  }
+
+  const handleToggleStatus = (targetId: number) => {
+    setAdList(
+      adList.map((item: IAdCard) => {
+        if (targetId !== item.id) return item
+
+        if (item.status === 'active') {
+          return { ...item, status: 'ended' }
+        }
+
+        if (item.status === 'ended') {
+          return { ...item, status: 'active' }
+        }
+
+        return item
+      })
+    )
   }
 
   return (
     <div className={cx(styles.card, isEditting && styles.isEditting)}>
-      <h4>{`${adType === 'web' ? '웹광고' : '앱광고'}_${title}`}</h4>
+      <h4>
+        <EditableBox defaultText={`${adType === 'web' ? '웹광고' : '앱광고'}_${title}`} isEditting={isEditting} />
+      </h4>
       <ul>
         <li>
           <div>상태</div>
-          <div>{status === 'active' ? '진행중' : '종료'}</div>
+          <div role='button' tabIndex={0} onClick={() => handleToggleStatus(id)}>
+            {status === 'active' ? '진행중' : '종료'}
+          </div>
         </li>
         <li>
           <div>광고 생성일</div>
@@ -44,7 +67,9 @@ const AdItem = ({ ad }: { ad: IAdCard }) => {
         </li>
         <li>
           <div>일 희망 예산</div>
-          <EditableBox defaultText={`${budget}만원`} isEditting={isEditting} />
+          <div>
+            <EditableBox defaultText={`${budget}만원`} isEditting={isEditting} />
+          </div>
         </li>
         <li>
           <div>광고 수익률</div>
