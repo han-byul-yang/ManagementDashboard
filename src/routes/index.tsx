3100f5465963ef5b9525293d106c215559fc93a9
lazy-sky <1cielo@naver.com>
2022-05-24 02:11:37 +0900

    Feat: Set react query in Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
+import { QueryClientProvider, QueryClient } from 'react-query'
 
 import NavBar from 'components/NavBar'
 import ConsoleBar from 'components/ConsoleBar'
@@ -9,6 +10,8 @@ import Management from './Management'
 import styles from './Routes.module.scss'
 
 const App = () => {
+  const queryClient = new QueryClient()
+
   return (
     <div className={styles.page}>
       <NavBar />
@@ -16,10 +19,12 @@ const App = () => {
         <ConsoleBar />
         <main className={styles.main}>
           <RecoilRoot>
-            <Routes>
-              <Route path='/' element={<DashBoard />} />
-              <Route path='management' element={<Management />} />
-            </Routes>
+            <QueryClientProvider client={queryClient}>
+              <Routes>
+                <Route path='/' element={<DashBoard />} />
+                <Route path='management' element={<Management />} />
+              </Routes>
+            </QueryClientProvider>
           </RecoilRoot>
         </main>
       </div>
