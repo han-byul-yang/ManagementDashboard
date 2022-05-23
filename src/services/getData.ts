6064fe608cd83f74c807c245048b8faca3008787
lazy-sky <1cielo@naver.com>
2022-05-24 03:07:00 +0900

    Feat: Make sleep function for a deliberate delay

--- src/services/getData.ts
+++ src/services/getData.ts
@@ -1,6 +1,13 @@
 import axios from 'axios'
 
+const sleep = (delay: number) => {
+  // eslint-disable-next-line no-promise-executor-return
+  return new Promise((res) => setTimeout(res, delay))
+}
+
 export const getAds = async () => {
+  await sleep(1000)
+
   const {
     data: { ads },
   } = await axios.get('data/adListData.json')
