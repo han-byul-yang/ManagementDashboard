85959a7ef15c82e83bea5067fef8092de185743b
lazy-sky <1cielo@naver.com>
2022-05-23 23:29:49 +0900

    Refactor: Separate AdItem from Management

---
+++ src/routes/Management/AdItem.tsx
@@ -0,0 +1,92 @@
+import { Dispatch, SetStateAction } from 'react'
+import cx from 'classnames'
+import dayjs from 'dayjs'
+
+import EditableBox from './EditableBox'
+
+import styles from './Management.module.scss'
+
+interface IAd {
+  id: number
+  adType: string
+  title: string
+  budget: number
+  status: string
+  startDate: string
+  endDate: string | null
+  report: {
+    cost: number
+    convValue: number
+    roas: number
+  }
+}
+
+interface IAdCard extends IAd {
+  isEditting: boolean
+}
+
+const AdItem = ({
+  ad,
+  adList,
+  setAdList,
+}: {
+  ad: IAdCard
+  adList: IAdCard[]
+  setAdList: Dispatch<SetStateAction<IAdCard[]>>
+}) => {
+  const {
+    id,
+    adType,
+    title,
+    budget,
+    status,
+    startDate,
+    endDate,
+    report: { cost, convValue, roas },
+    isEditting,
+  } = ad
+
+  const handleEditBtnClick = (targetId: number) => {
+    setAdList(adList.map((item: any) => (targetId === item.id ? { ...item, isEditting: !ad.isEditting } : item)))
+  }
+
+  return (
+    <div className={cx(styles.card, isEditting && styles.isEditting)}>
+      <h4>{`${adType === 'web' ? '웹광고' : '앱광고'}_${title}`}</h4>
+      <ul>
+        <li>
+          <div>상태</div>
+          <div>{status === 'active' ? '진행중' : '종료'}</div>
+        </li>
+        <li>
+          <div>광고 생성일</div>
+          <div>
+            {dayjs(startDate).format('YYYY-MM-DD')}
+            {endDate && `(${dayjs(endDate).format('YYYY-MM-DD')})`}
+          </div>
+        </li>
+        <li>
+          <div>일 희망 예산</div>
+          <EditableBox defaultText={`${budget}만원`} isEditting={isEditting} />
+        </li>
+        <li>
+          <div>광고 수익률</div>
+          <div>{roas.toLocaleString()}%</div>
+        </li>
+        <li>
+          <div>매출</div>
+          <div>{convValue.toLocaleString()}원</div>
+        </li>
+        <li>
+          <div>광고 비용</div>
+          <div>{cost.toLocaleString()}원</div>
+        </li>
+      </ul>
+      <button type='button' onClick={() => handleEditBtnClick(id)} className={styles.editBtn}>
+        {isEditting ? '저장하기' : '수정하기'}
+      </button>
+    </div>
+  )
+}
+
+export default AdItem
--- src/routes/Management/index.tsx
+++ src/routes/Management/index.tsx
@@ -1,17 +1,9 @@
 import { useState, useEffect, ChangeEvent } from 'react'
-import cx from 'classnames'
-import dayjs from 'dayjs'
 
 import { getAds } from 'services/getData'
-import EditableBox from './EditableBox'
 
 import styles from './Management.module.scss'
-
-/* TODO
-- 기본 레이아웃
-- 카드 편집
-- 카드 생성 
-*/
+import AdItem from './AdItem'
 
 interface IAd {
   id: number
@@ -62,10 +54,6 @@ const Management = () => {
     setAdList((prev) => [...prev, newAd])
   }
 
-  const handleEditBtnClick = (id: number) => {
-    setAdList(adList.map((ad) => (id === ad.id ? { ...ad, isEditting: !ad.isEditting } : ad)))
-  }
-
   const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
     setFilter(e.target.value)
   }
@@ -94,57 +82,9 @@ const Management = () => {
 
             return ad
           })
-          .map((ad) => {
-            const {
-              id,
-              adType,
-              title,
-              budget,
-              status,
-              startDate,
-              endDate,
-              report: { cost, convValue, roas },
-              isEditting,
-            } = ad
-
-            return (
-              <div key={id} className={cx(styles.card, isEditting && styles.isEditting)}>
-                <h4>{`${adType === 'web' ? '웹광고' : '앱광고'}_${title}`}</h4>
-                <ul>
-                  <li>
-                    <div>상태</div>
-                    <div>{status === 'active' ? '진행중' : '종료'}</div>
-                  </li>
-                  <li>
-                    <div>광고 생성일</div>
-                    <div>
-                      {dayjs(startDate).format('YYYY-MM-DD')}
-                      {endDate && `(${dayjs(endDate).format('YYYY-MM-DD')})`}
-                    </div>
-                  </li>
-                  <li>
-                    <div>일 희망 예산</div>
-                    <EditableBox defaultText={`${budget}만원`} isEditting={isEditting} />
-                  </li>
-                  <li>
-                    <div>광고 수익률</div>
-                    <div>{roas.toLocaleString()}%</div>
-                  </li>
-                  <li>
-                    <div>매출</div>
-                    <div>{convValue.toLocaleString()}원</div>
-                  </li>
-                  <li>
-                    <div>광고 비용</div>
-                    <div>{cost.toLocaleString()}원</div>
-                  </li>
-                </ul>
-                <button type='button' onClick={() => handleEditBtnClick(id)} className={styles.editBtn}>
-                  {isEditting ? '저장하기' : '수정하기'}
-                </button>
-              </div>
-            )
-          })}
+          .map((ad) => (
+            <AdItem key={ad.id} ad={ad} adList={adList} setAdList={setAdList} />
+          ))}
       </div>
     </div>
   )
