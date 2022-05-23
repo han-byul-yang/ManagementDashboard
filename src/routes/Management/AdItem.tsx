be3960851ce05a3d5f04f7f7f590103d1c849c19
lazy-sky <1cielo@naver.com>
2022-05-24 01:33:08 +0900

    Feat: Process number data in AdItem

--- src/routes/Management/AdItem.tsx
+++ src/routes/Management/AdItem.tsx
@@ -3,12 +3,15 @@ import cx from 'classnames'
 import dayjs from 'dayjs'
 
 import { adListState } from 'store/atoms'
+import { processBudget } from './utils'
 import { IAdCard } from 'types/ad'
 import EditableBox from './EditableBox'
+import { EditIcon } from 'assets/svgs'
 
 import styles from './Management.module.scss'
 
 const AdItem = ({ ad }: { ad: IAdCard }) => {
+  const [adList, setAdList] = useRecoilState<IAdCard[]>(adListState)
   const {
     id,
     adType,
@@ -20,8 +23,8 @@ const AdItem = ({ ad }: { ad: IAdCard }) => {
     report: { cost, convValue, roas },
     isEditting,
   } = ad
-
-  const [adList, setAdList] = useRecoilState<IAdCard[]>(adListState)
+  const processedConvValue = `${Math.round(convValue / 10000).toLocaleString()}만원`
+  const processedCost = `${Math.round(cost / 10000).toLocaleString()}만원`
 
   // 어떤 요소까지 수정이 가능한지 주어진 데이터만으로는 알기 어려워 임의적으로 제목과 예산만 수정 가능한 요소라고 판단
   const handleEditBtnClick = (targetId: number) => {
@@ -57,6 +60,7 @@ const AdItem = ({ ad }: { ad: IAdCard }) => {
         <li>
           <div>상태</div>
           <div role='button' tabIndex={0} onClick={() => handleToggleStatus(isEditting, id)}>
+            {isEditting && <EditIcon />}
             {status === 'active' ? '진행중' : '종료'}
           </div>
         </li>
@@ -70,7 +74,7 @@ const AdItem = ({ ad }: { ad: IAdCard }) => {
         <li>
           <div>일 희망 예산</div>
           <div>
-            <EditableBox defaultText={`${budget}만원`} isEditting={isEditting} />
+            <EditableBox defaultText={`${processBudget(budget)}`} isEditting={isEditting} />
           </div>
         </li>
         <li>
@@ -79,11 +83,11 @@ const AdItem = ({ ad }: { ad: IAdCard }) => {
         </li>
         <li>
           <div>매출</div>
-          <div>{convValue.toLocaleString()}원</div>
+          <div>{processedConvValue}</div>
         </li>
         <li>
           <div>광고 비용</div>
-          <div>{cost.toLocaleString()}원</div>
+          <div>{processedCost}</div>
         </li>
       </ul>
       <button type='button' onClick={() => handleEditBtnClick(id)} className={styles.editBtn}>
