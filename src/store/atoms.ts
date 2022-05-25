689fd8d2f3db7c305f2d19b0c0f1a696c58d1c98
lazy-sky <1cielo@naver.com>
2022-05-26 02:39:29 +0900

    Feat: 리코일 atom 셋팅 - 근영

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -1,7 +1,23 @@
 import { atom } from 'recoil'
 import { IAdCard } from 'types/ad'
+import { IStatusData } from 'types/types'
 
 export const adListState = atom<IAdCard[]>({
   key: 'adList',
+  default: [],
+})
+
+interface PickedDate {
+  start: Date
+  end: Date
+}
+
+export const pickedDate = atom<PickedDate>({
+  key: 'pickedDate',
+  default: { start: new Date('2022-03-01'), end: new Date('2022-03-07') },
+})
+
+export const integratedAdData = atom<IStatusData[]>({
+  key: 'integratedAdData',
   default: [],
 })
